03480f76b865e625e01c977491f1bc6fe41591dc
suzenhalil <suzenhalil@hotmail.com>
2022-04-22 17:02:44 +0300

    Proje dosyası ekle.

---
+++ EvdeDers/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace EvdeDers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region While2
+            //int sayi;
+            //Console.WriteLine("Bir Sayı Giriniz.: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+
+            //while (sayi % 2 == 0)
+            //{
+
+            //    Console.WriteLine("Bir Sayı Giriniz.: ");
+            //    sayi = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //Console.WriteLine("Tek Sayı Girdiniz.");
+            #endregion
+
+            #region Faktöriyel
+
+            //Console.WriteLine("Sayı Girinz.: ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //int faktor = 1;
+
+
+            //while (sayi >1)
+            //{
+            //    faktor = faktor * sayi;
+            //    sayi--;
+            //}
+            //Console.WriteLine("Girdiğiniz sayının faktöriyeli: " + faktor);
+
+
+            #endregion
+
+            #region Diziler
+
+            //string[] sehirler = new string[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("Sehir: ");
+            //    sehirler[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine("****Sehirler Listesi****");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(sehirler[i]);
+
+            #endregion
+
+            #region Diziler2
+            #region Örnek1
+
+            //int[] sayilar = new int[5];
+
+            //for (int i = 0; i < 5; i++)
+
+            //{
+            //    Console.WriteLine((i+1) + ". Sayı Giriniz.: ");
+            //    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+
+            //}
+            //int enbuyuk;
+            //enbuyuk = sayilar[0];
+            //for (int i = 1; i < 5; i++)
+            //{
+            //    if (enbuyuk < sayilar[i])
+            //    {
+            //        enbuyuk = sayilar[i];
+
+            //    }
+
+            //}
+            //Console.WriteLine(enbuyuk);
+
+            #endregion
+
+            #region Örnek2
+
+            //string[] isim = new string[3];
+            //double[] s1 = new double[3];
+            //double[] s2 = new double[3];
+            //double[] ort = new double[3];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("İsminizi Giriniz.: ");
+            //    isim[i] = Console.ReadLine();
+
+            //    Console.WriteLine("1.Sınav Giriniz.: ");
+            //    s1[i] = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.WriteLine("2.Sınav Giriniz.: ");
+            //    s2[i] = Convert.ToDouble(Console.ReadLine());
+
+            //    ort[i] = (s1[i] + s2[i]) / 2;
+
+            //}
+            //Console.Write(" Öğrenci     Sınav1      Sınav2      Ortalama ");
+            //Console.WriteLine();
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(" " + isim[i] + "     " + s1[i] + "      " + s2[i] + "      " + ort[i]);
+
+            //}
+
+            #endregion
+
+            #endregion
+
+        }
+
+
+
+    }
+}
+
